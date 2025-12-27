3fb0825cfe12584866a16df568a89733e4c9ca05
Zeeshan Qureshi <pmz5164300@gmail.com>
2025-12-27 18:41:09 +0500

    Added transfer from function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import {ERC20_ABI} from "./abi.js";
 let provider;
 let signer;
+let tokenContract;
+
 const tokenAddress = "0x71B0Cf1Fa252787818F692e136bc91C99855a059";
 
 async function connectWallet() {
@@ -8,23 +10,49 @@ async function connectWallet() {
         alert ("Metamask not found");
         return;
     }
+    const SEPOLIA_CHAIN_ID = '0xaa36a7'; 
+    try{
     provider = new ethers.BrowserProvider(window.ethereum);
     await provider.send("eth_requestAccounts", []);
-    signer = await provider.getSigner();
+    const network = await provider.getNetwork();
+    const currentChainId = "0x" + network.chainId.toString(16);
 
-    const address = await signer.getAddress();
-    document.getElementById("account").innerText = address;
-        loadTokenData();}
+ if (currentChainId !== SEPOLIA_CHAIN_ID) {
+            try {
+                // 3. Request switch to Sepolia
+                await window.ethereum.request({
+                    method: 'wallet_switchEthereumChain',
+                    params: [{ chainId: SEPOLIA_CHAIN_ID }],
+                });
+            } catch (switchError) {
+                // Error 4902 means Sepolia isn't added to their MetaMask
+                if (switchError.code === 4902) {
+                    await addSepoliaNetwork();
+                } else {
+                    throw switchError;
+                }
+            }
+        }
+
+         signer = await provider.getSigner();
+        tokenContract = new ethers.Contract(tokenAddress, ERC20_ABI, signer);
+
+      const address = await signer.getAddress();
+      document.getElementById("account").innerText = address;
+        loadTokenData();
+
+    } catch (error) {
+        console.error("Connection failed", error);
+    }
+}
         
-
 async function loadTokenData(){ 
-let token = new ethers.Contract(tokenAddress, ERC20_ABI, provider); 
 
-   document.getElementById("tokenName").innerText = await token.name();
-   document.getElementById("tokenSymbol").innerText = await token.symbol();
-   document.getElementById("tokenDecimals").innerText = (await token.decimals()).toString();
+   document.getElementById("tokenName").innerText = await tokenContract.name();
+   document.getElementById("tokenSymbol").innerText = await tokenContract.symbol();
+   document.getElementById("tokenDecimals").innerText = (await tokenContract.decimals()).toString();
 
-   const balance = await token.balanceOf(await signer.getAddress());
+   const balance = await tokenContract.balanceOf(await signer.getAddress());
    document.getElementById("balance").innerText = 
    ethers.formatEther(balance);
 }
@@ -33,7 +61,6 @@ let token = new ethers.Contract(tokenAddress, ERC20_ABI, provider);
 async function transfer() {
     const amountToTransfer  = document.getElementById("amount").value;
     const address =document.getElementById("address").value ;
-    const tokenContract = new ethers.Contract(tokenAddress, ERC20_ABI,signer);
 
     const amount = ethers.parseUnits(amountToTransfer, 18);
     try{
@@ -54,7 +81,6 @@ async function transfer() {
    async function Approve() {
     const amountToApprove = document.getElementById("amountforSpender").value;
     const address = document.getElementById("spenderAddr").value;
-    const tokenContract = new ethers.Contract(tokenAddress, ERC20_ABI,signer);
 
     const amount = ethers.parseUnits(amountToApprove, 18)
     try{
@@ -68,14 +94,30 @@ async function transfer() {
  document.getElementById("approveBtn").onclick = Approve;
    
   async function checkAllowance() {
-     let token = new ethers.Contract(tokenAddress, ERC20_ABI, provider); 
     const approverAddr = document.getElementById("approver").value;
     const spenderAdr = document.getElementById("spender").value;
 try{
-    const allowanceAmount = await token.allowance(approverAddr, spenderAdr);
+    const allowanceAmount = await tokenContract.allowance(approverAddr, spenderAdr);
     document.getElementById("allowance").innerText = ethers.formatUnits(allowanceAmount, 18);
     } catch (error){
         console.error(error);
     }
     }
     document.getElementById("allowanceBtn").onclick = checkAllowance;
+    
+    async function transferFrom() {
+    const fromAddr = document.getElementById("_from").value;
+    const toAddr = document.getElementById("_to").value;
+    const amountToSend = document.getElementById("_amount").value;
+
+    const amount = ethers.parseUnits(amountToSend, 18)
+   try{
+    const tx = await tokenContract.transferFrom(fromAddr, toAddr, amount);
+    await tx.wait();
+    console.log("Transaction Confirmed: ",tx.hash);
+   } catch (error){
+    console.error(error);
+   }
+}
+
+document.getElementById("transferFromBtn").onclick = transferFrom;
